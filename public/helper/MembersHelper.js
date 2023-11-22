92af4c2b05ed034dfcbe40f45353585c8e45da4c
h-ch22 <ckdwls9460@gmail.com>
2023-11-22 12:44:19 +0900

    feat: RadioButton on Members

--- public/helper/MembersHelper.js
+++ public/helper/MembersHelper.js
@@ -18,8 +18,9 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const members = [];
 
+var selectedType = MembersType.PROFESSOR;
+
 async function getMembers() {
-    console.log('loading');
     const snapshot = await getDocs(collection(db, "Members"));
     snapshot.forEach((doc) => {
         const degree = doc.get("degree");
@@ -68,90 +69,90 @@ function visitPage(url){
 }
 
 async function show() {
-    const div_professor = document.querySelector("#div_professor");
-    const pre_professor_ul = document.querySelector("#div_professor ul");
+    const div_members = document.querySelector("#div_members");
+    const pre_members_ul = document.querySelector("#div_members ul");
 
-    const div_student = document.querySelector("#div_student");
-    const pre_student_ul = document.querySelector("#div_student ul");
-
-    const div_alumni = document.querySelector("#div_alumni");
-    const pre_alumni_ul = document.querySelector("#div_alumni ul");
-
-    if (pre_professor_ul) {
-        pre_professor_ul.remove();
+    if (pre_members_ul) {
+        pre_members_ul.remove();
     }
-
-    if(pre_student_ul){
-        pre_student_ul.remove();
-    }
-
-    if(pre_alumni_ul){
-        pre_alumni_ul.remove();
-    }
-
 
     members.forEach(function (member) {
-        const ul = document.createElement("ul");
+        if(member.cat == selectedType){
+            const ul = document.createElement("ul");
 
-        const li = document.createElement("li");
-        li.id = "li_member";
-        
-        const degree = document.createElement("p");
-        const img = document.createElement("img");
-        const txt_name = document.createElement("h1");
-        const txt_email = document.createElement("h4");
-        const txt_dept = document.createElement("h4");
-        const txt_tel = document.createElement("h4");
-
-        degree.id = "txt_degree";
-        degree.innerText = member.degree;
-
-        img.src = member.profile;
-        img.id = "img_profile";
-
-        txt_name.innerText = member.name;
-        txt_name.id = "txt_name";
-
-        txt_email.innerText = member.email;
-        txt_email.id = "txt_email";
-
-        txt_dept.innerText = member.dept;
-        txt_dept.id = "txt_dept";
-
-        txt_tel.innerText = member.tel;
-        txt_tel.id = "txt_tel";
-
-        li.appendChild(img);
-        li.appendChild(txt_name);
-        li.appendChild(degree);
-        li.appendChild(txt_email);
-        li.appendChild(txt_dept);
-        li.appendChild(txt_tel);
-
-        if(member.site != null){
-            const btn_web = document.createElement("button");
-            btn_web.className = "button-18";
-            btn_web.innerText = "Visit Website";
-            btn_web.role = "button";
-            btn_web.onclick=visitPage.bind(null, member.site);
-            li.appendChild(btn_web);
+            const li = document.createElement("li");
+            li.id = "li_member";
+            
+            const degree = document.createElement("p");
+            const img = document.createElement("img");
+            const txt_name = document.createElement("h1");
+            const txt_email = document.createElement("h4");
+            const txt_dept = document.createElement("h4");
+            const txt_tel = document.createElement("h4");
+    
+            degree.id = "txt_degree";
+            degree.innerText = member.degree;
+    
+            img.src = member.profile;
+            img.id = "img_profile";
+    
+            txt_name.innerText = member.name;
+            txt_name.id = "txt_name";
+    
+            txt_email.innerText = member.email;
+            txt_email.id = "txt_email";
+    
+            txt_dept.innerText = member.dept;
+            txt_dept.id = "txt_dept";
+    
+            txt_tel.innerText = member.tel;
+            txt_tel.id = "txt_tel";
+    
+            li.appendChild(img);
+            li.appendChild(txt_name);
+            // li.appendChild(degree);
+            li.appendChild(txt_email);
+            li.appendChild(txt_dept);
+            li.appendChild(txt_tel);
+    
+            if(member.site != null){
+                const btn_web = document.createElement("button");
+                btn_web.className = "button-18";
+                btn_web.innerText = "Visit Website";
+                btn_web.role = "button";
+                btn_web.onclick=visitPage.bind(null, member.site);
+                li.appendChild(btn_web);
+            }
+    
+            ul.appendChild(li);
+    
+            div_members.appendChild(ul);
         }
-
-        ul.appendChild(li);
-
-        switch(member.cat){
-            case MembersType.PROFESSOR:
-                div_professor.appendChild(ul);
-                break;
-
-            case MembersType.STUDENT:
-                div_student.appendChild(ul);
-                break;
-        }
-
     });
-
-
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    var radioButtons = document.getElementsByName("radio");
+
+    radioButtons.forEach((btn) => {
+        btn.addEventListener('click', function(){
+            switch(btn.id){
+                case "btn_professor":
+                    selectedType = MembersType.PROFESSOR;
+                    break;
+        
+                case "btn_students":
+                    selectedType = MembersType.STUDENT;
+                    break;
+        
+                case "btn_alumni":
+                    selectedType = MembersType.ALUMNI;
+                    break;
+            }
+
+            show();
+        });
+    });
+})
 
 getMembers();
