db41c9c7756ef81b2688f3e312ff057087893031
h-ch22 <ckdwls9460@naver.com>
2024-01-24 16:59:14 +0900

    feat: geocoder in contact

--- helper/ContactHelper.js
+++ helper/ContactHelper.js
@@ -24,6 +24,9 @@ const storage = getStorage(app);
 var contents = "";
 var tel = "";
 var email = "";
+var addressSearched = false;
+var oldAddress = "";
+var latLng = "";
 
 const editor = document.getElementById('editor');
 const btnBold = document.getElementById('btn-bold');
@@ -81,11 +84,13 @@ function isStyle(style) {
     return document.queryCommandState(style);
 }
 
-async function update(contents, email, tel){
+async function update(contents, email, tel, address){
     const data = {
         "contents": contents,
         "tel": tel,
-        "email": email
+        "email": email,
+        "address": address,
+        "latLng": latLng
     };
 
     await updateDoc(doc(db, "Contact", "Introduction"), data)
@@ -118,7 +123,12 @@ async function checkAdminPermission(){
             btn_edit.addEventListener('click', function(){
                 const field_email = document.getElementById("field_email");
                 const field_tel = document.getElementById("field_tel");
-    
+                const field_address = document.getElementById("field_address");
+                const btn_searchAddress = document.getElementById("btn_searchAddress");
+                const ic_search = document.getElementById("ic_search");
+
+                field_address.value = oldAddress == null ? "" : oldAddress;
+
                 field_email.value = email;
                 field_tel.value = tel;
                 editor.innerHTML = introductionContents.innerHTML;
@@ -149,6 +159,39 @@ async function checkAdminPermission(){
 
                 btnImage.addEventListener('click', function(){
                     imageSelector.click();
+                })
+
+                btn_searchAddress.addEventListener('click', function(){
+                    naver.maps.Service.geocode({
+                        query: field_address.value
+                    }, function(status, response) {
+                        if (status === naver.maps.Service.Status.ERROR) {
+                            return alert('An error occured while searching address.');
+                        }
+                        if (response.v2.meta.totalCount === 0) {
+                            return alert('Please enter correct address.');
+                        }
+
+                        const item = response.v2.addresses[0];
+
+                        alert(`Searched address: ${item.roadAddress}\nIf the output address matches the intended address, continue.\nIf it doesn't match, please enter a more detailed address.`)
+                        
+                        oldAddress = item.roadAddress;
+                        field_address.value = item.roadAddress;
+                        latLng = `${item.x}, ${item.y}`;
+                        ic_search.className = "fa fa-check";
+                        addressSearched = true;
+                    });
+                })
+
+                field_address.addEventListener('change', function(){
+                    if(field_address.value != oldAddress){
+                        addressSearched = false;
+                        ic_search.className = "fa fa-search";
+                    } else{
+                        addressSearched = true;
+                        ic_search.className = "fa fa-check";
+                    }
                 })
 
                 imageSelector.addEventListener('change', async function (e) {
@@ -201,10 +244,12 @@ async function checkAdminPermission(){
                 const field_email = document.getElementById("field_email");
                 const field_tel = document.getElementById("field_tel");
 
-                if(editor.innerHTML == "" || field_email.value == "" || field_tel.value == ""){
+                if(editor.innerHTML == "" || field_email.value == "" || field_tel.value == "" || field_address.value == ""){
                     alert('Please write down all fields.');
-                } else{
-                    update(editor.innerHTML, field_email.value, field_tel.value);
+                } else if(field_address.value != oldAddress && !addressSearched){
+                    alert('Please search address');
+                }else{
+                    update(editor.innerHTML, field_email.value, field_tel.value, field_address.value);
                 }
             });
         }
@@ -221,6 +266,8 @@ async function getIntroduction() {
         contents = docSnap.data()["contents"];
         tel = docSnap.data()["tel"];
         email = docSnap.data()["email"];
+        oldAddress = docSnap.data()["address"] == null ? "" : docSnap.data()["address"];
+        latLng = docSnap.data()["latLng"];
 
         btn_email.innerHTML = `${email} &#128203`;
         btn_phone.innerHTML = `${tel} &#128203`;
@@ -235,6 +282,30 @@ async function getIntroduction() {
 
         const introductionContents = document.getElementById("introductionContents")
         introductionContents.innerHTML = contents == null ? "" : contents;
+
+        const latLng_splited = latLng.split(", ");
+
+        var mapOptions = {
+            center: new naver.maps.LatLng(parseFloat(latLng_splited[1]), parseFloat(latLng_splited[0])),
+            zoom: 15
+        };
+
+        var map = new naver.maps.Map('map', mapOptions);
+        var markerOptions = {
+            position: new naver.maps.LatLng(parseFloat(latLng_splited[1]), parseFloat(latLng_splited[0])),
+            map: map,
+            icon: {
+            url: './res/fav/apple-icon.png',
+            size: new naver.maps.Size(96, 96),
+            scaledSize: new naver.maps.Size(96, 96),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(48, 96)
+            },
+            title: 'Smart Biophotonics Lab.',
+            animation: naver.maps.Animation.DROP
+        };
+
+        var marker = new naver.maps.Marker(markerOptions);
     }
 }
 
